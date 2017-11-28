59bb326c5e65082eeedd088cf22fd471b137232f
Rabia <rabiaozturk306@gmail.com>
2017-11-28 11:48:37 +0300

    Programın başlığı değiştirildi.

--- widget.cpp
+++ widget.cpp
@@ -13,7 +13,7 @@ Widget::Widget(QWidget *parent) :
    // mSystemTrayIcon =new QSystemTrayIcon(this);
 
     connect(ui->pushButton,SIGNAL(clicked()), this, SLOT(display()));
-    setWindowTitle(tr("Intern HW"));
+    setWindowTitle(tr("Intern HW Başlığı"));
 }
 
 Widget::~Widget()
